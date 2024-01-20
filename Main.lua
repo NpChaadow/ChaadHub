839b1a93eaa5df3b5b60b93f98a83b2c530edf10
NpChaadow <90306736+NpChaadow@users.noreply.github.com>
2024-01-20 15:00:39 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -283,10 +283,8 @@ AutoBuyButton.MouseButton1Click:Connect(function()
 	while AutoBuy do
 		wait(1)
 		for i,v in pairs(PlayerTycoon.Buttons:GetChildren())do
-			print(v.Button.Color)
-			if v.Button.Color == Color3.new(0.223529,0.65098,0.156863) then
-				character:MoveTo(v.Button.Position)
-			end
+			print(v)
+			character:MoveTo(v.Button.Position)
 		end
 	end
 	
