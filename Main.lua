d057f61bd51c37c9d3c0cf7eb5f2495045eb0e7f
NpChaadow <90306736+NpChaadow@users.noreply.github.com>
2024-01-25 22:33:15 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -210,7 +210,7 @@ end
 local MenuFrame = CreateTabFrame(.5,.5, .7,.8,"Menu")
 MenuFrame.Parent = ScreenGui
 
-local MenuVersionLabel = CreateTextLabel(.6,.5,.5,1,"MenuVersionLabel","V0.0.3c",Color3.new(0.384314, 0.384314, 0.384314),Color3.new(1, 1, 1))
+local MenuVersionLabel = CreateTextLabel(.6,.5,.5,1,"MenuVersionLabel","V0.0.3d",Color3.new(0.384314, 0.384314, 0.384314),Color3.new(1, 1, 1))
 MenuVersionLabel.Parent = MenuFrame.TopBar
 
 local BrowserFrame = CreateTabFrame(.5,.5,.7,.8,"BrowserFrame")
@@ -462,12 +462,9 @@ AutoCrateButton.MouseButton1Click:Connect(function()
 	while AutoCrate do
 		if CrateFound then
 			
-			GoToPoint(character.PrimaryPart.Position,CratePos.Position,40,0.1)
-
-			character.PrimaryPart.Anchored = true
-
-			GoToPoint(character.PrimaryPart.Position,CratePos.Position - Vector3.new(0,10,0),10,0.1)
+			GoToPoint(character.PrimaryPart.Position,CratePos.Position - Vector3.new(0,10,0),40,0.1)
 				
+			character.PrimaryPart.Anchored = true	
 			wait(35)
 			character.PrimaryPart.Anchored = false
 			CrateFound = false
