eb8599cf0f20552761c56c18a64992c720726a37
NpChaadow <90306736+NpChaadow@users.noreply.github.com>
2024-02-07 23:17:59 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -216,7 +216,7 @@ function CreateTabFrame(x,y,xsize,ysize, Name)
 	ContentFrame.BackgroundTransparency = 1
 	ContentFrame.BorderSizePixel = 0
 	
-	local HubLabel = CreateTextLabel(0.4,0.070,0.5,0.16,"HubNameLabel","Chaad Hub v.0.2.0b",Color3.new(0.458824, 0.458824, 0.458824),Color3.new(1, 1, 1))
+	local HubLabel = CreateTextLabel(0.4,0.070,0.5,0.16,"HubNameLabel","Chaad Hub v.0.2.0c",Color3.new(0.458824, 0.458824, 0.458824),Color3.new(1, 1, 1))
 	HubLabel.BackgroundTransparency = 0.9
 	HubLabel.Parent = Frame
 	
@@ -847,6 +847,7 @@ end)
 function LookInFiles(Object:Instance)
 
 	if #Object:GetChildren() <= 0 then
+		print("a")
 		return
 	end	
 
@@ -871,6 +872,8 @@ function LookInFiles(Object:Instance)
 		end
 
 	end
+	
+	print("End")
 
 end
 
