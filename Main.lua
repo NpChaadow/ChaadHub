a4bd6e993abf58447b240d944e7ddfb46a0aa4bf
NpChaadow <90306736+NpChaadow@users.noreply.github.com>
2024-12-25 02:27:50 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -19,7 +19,7 @@ local Mouse = Player:GetMouse()
 local UserInputService = game:GetService("UserInputService")
 
 local ScreenGui = Instance.new('ScreenGui')
-ScreenGui.Parent = Player.PlayerGui
+ScreenGui.Parent = Game.StarterGui
 
 local ButtonFrameOutline = Instance.new("Frame")
 ButtonFrameOutline.Parent = ScreenGui
@@ -260,7 +260,7 @@ function CreateTabFrame(x,y,xsize,ysize, Name)
 	ContentFrame.BackgroundTransparency = 1
 	ContentFrame.BorderSizePixel = 0
 
-	local HubLabel = CreateTextLabel(0.4,0.070,0.5,0.16,"HubNameLabel","Chaad Hub v.0.2.1c",Color3.new(0.458824, 0.458824, 0.458824),Color3.new(1, 1, 1))
+	local HubLabel = CreateTextLabel(0.4,0.070,0.5,0.16,"HubNameLabel","Chaad Hub v.0.2.1d",Color3.new(0.458824, 0.458824, 0.458824),Color3.new(1, 1, 1))
 	HubLabel.BackgroundTransparency = 0.9
 	HubLabel.Parent = Frame
 
