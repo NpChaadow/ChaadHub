1c45a903ee36fac5de6af0d80c78219a6816210d
NpChaadow <90306736+NpChaadow@users.noreply.github.com>
2024-12-29 01:56:37 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -260,7 +260,7 @@ function CreateTabFrame(x,y,xsize,ysize, Name)
 	ContentFrame.BackgroundTransparency = 1
 	ContentFrame.BorderSizePixel = 0
 
-	local HubLabel = CreateTextLabel(0.4,0.070,0.5,0.16,"HubNameLabel","Chaad Hub v.0.2.3c",Color3.new(0.458824, 0.458824, 0.458824),Color3.new(1, 1, 1))
+	local HubLabel = CreateTextLabel(0.4,0.070,0.5,0.16,"HubNameLabel","Chaad Hub v.0.2.3d",Color3.new(0.458824, 0.458824, 0.458824),Color3.new(1, 1, 1))
 	HubLabel.BackgroundTransparency = 0.9
 	HubLabel.Parent = Frame
 
@@ -727,7 +727,8 @@ function autoBuy()
 
 		end
 	end
-
+	wait(.25)
+	autoBuy()
 end
 
 --Auto Collect
